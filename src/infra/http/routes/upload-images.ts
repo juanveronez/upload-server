dfe9cff7ac6f52579f06e36a604fb613400e336a
Juan Veronez <juan.monteirov@gmail.com>
2025-07-19 21:46:45 -0300

    feat: create upload image function and use db and schema to do it

--- src/infra/http/routes/upload-images.ts
+++ src/infra/http/routes/upload-images.ts
@@ -1,6 +1,7 @@
 import { randomUUID } from 'node:crypto'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import z from 'zod/v4'
+import { uploadImage } from '@/app/functions/upload-image'
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
   server.post(
@@ -11,20 +12,25 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
         consumes: ['multipart/form-data'],
         response: {
           201: z.object({ uploadId: z.uuidv7() }),
-          409: z
-            .object({ message: z.string() })
-            .describe('Upload already exists.'),
+          400: z.object({ message: z.string() }),
         },
       },
     },
     async (request, reply) => {
       const TWO_MEGABYTES = 1024 * 1024 * 2
 
-      const uploadedMultipart = await request.file({
+      const uploadedFile = await request.file({
         limits: { fileSize: TWO_MEGABYTES },
       })
 
-      console.log(uploadedMultipart)
+      if (!uploadedFile)
+        return reply.status(400).send({ message: 'File is required' })
+
+      await uploadImage({
+        fileName: uploadedFile.filename,
+        contentType: uploadedFile.mimetype,
+        contentStream: uploadedFile.file,
+      })
 
       return reply.status(201).send({ uploadId: randomUUID() })
     }
